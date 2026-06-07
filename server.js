dc79d35af82eba5f8ce2821ac63b862ca0dc5e21
Cynthilizy <cynthilizy@gmail.com>
2026-06-07 18:09:16 +0300

    update db connection string

--- server.js
+++ server.js
@@ -21,12 +21,20 @@ const app = express();
 const port = 5005;
 const saltingRounds = 10;
 
-const db = new Pool({
+//for dev testing
+/*const db = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+});*/
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 //middle ware
