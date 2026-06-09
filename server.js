5cd92ac27e77f835297fa2b189f73bb5625c09d7
Cynthilizy <cynthilizy@gmail.com>
2026-06-09 18:26:07 +0300

    middleware debug

--- server.js
+++ server.js
@@ -73,6 +73,9 @@ app.get('/', (req, res) => {
 const auth = (req, res, next) => {
   const token = req.cookies.token;
 
+  console.log('cookies:', req.cookies);
+  console.log('token:', req.cookies?.token);
+
   if (!token) return res.sendStatus(401);
 
   try {
